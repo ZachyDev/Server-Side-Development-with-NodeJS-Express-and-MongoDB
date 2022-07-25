109bae34b729abd545291c26742a54ad01fa4190
= <=>
2022-07-26 00:32:48 +0300

    Node-Mongodb test connection to the server

--- Week-2/node-mongo/index.js
+++ Week-2/node-mongo/index.js
@@ -1,38 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'conFusion';
+let url = 'mongodb://localhost:27017';
+const dbName = 'Confusion';
 
 MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
+    assert.equal(err,null);
 
-    console.log('Connected correctly to server');
+    console.log('Connected properly to the server');
 
     const db = client.db(dbName);
     const collection = db.collection('dishes');
 
-    // insert data into a collection
-    collection.insertOne({
-        "name": "Burger",
-        "description": "Spanish Burger"
-    }, (err, result) => {
-        assert.equal(err, null);
-        
-        console.log("After insert: \n");
+    collection.insertOne({ "name": "Spanish Burger", "description": "Spanish burger costs $250"},(err,result) => {
+        assert.equal(err,null);
+
+        console.log('After insert:\n');
         console.log(result.ops);
 
         collection.find({}).toArray((err,docs) => {
-            assert.equal(err,null);
-
+            assert.equal(err,null)
+            
             console.log('Found:\n');
             console.log(docs);
 
             db.dropCollection('dishes', (err,result) => {
                 assert.equal(err,null);
-
                 client.close();
-            })
         })
+        });
     })
 })
